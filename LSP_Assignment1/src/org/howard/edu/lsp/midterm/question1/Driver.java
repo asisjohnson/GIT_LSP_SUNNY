a811abd02459601f72d922fe9932c5710c54c65a
asisjohnson <asisjohnson@localhost>
2024-03-14 20:11:57 -0400

    Updated Question 1 with JavaDocs

--- LSP_Assignment1/src/org/howard/edu/lsp/midterm/question1/Driver.java
+++ LSP_Assignment1/src/org/howard/edu/lsp/midterm/question1/Driver.java
@@ -1,6 +1,14 @@
 package org.howard.edu.lsp.midterm.question1;
 
+/**
+ * Test class for SecurityOps.
+ */
 public class Driver {
+    /**
+     * Main method to test the encryption operation.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         String input = "I love CSCI363";
         String encryptedText = SecurityOps.encrypt(input);
--- LSP_Assignment1/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
+++ LSP_Assignment1/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
@@ -1,26 +1,32 @@
 package org.howard.edu.lsp.midterm.question1;
 
+/**
+ * Provides encryption operations.
+ */
 public class SecurityOps {
+    /**
+     * Encrypts the given text by rearranging characters.
+     *
+     * @param text the text to encrypt
+     * @return the encrypted text
+     */
     public static String encrypt(String text) {
-        // Initialize two StringBuilder objects for even and odd characters
-        StringBuilder evenChars = new StringBuilder();
-        StringBuilder oddChars = new StringBuilder();
+        StringBuilder encryptedText = new StringBuilder();
 
-        // Iterate through each character in the input text
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            // Check if the character is a letter or digit (ignore punctuation and whitespace)
-            if (Character.isLetterOrDigit(c)) {
-                // Append even-indexed characters to evenChars StringBuilder
-                if (i % 2 == 0) {
-                    evenChars.append(c);
-                } else { // Append odd-indexed characters to oddChars StringBuilder
-                    oddChars.append(c);
-                }
+        // Append even-indexed characters
+        for (int i = 0; i < text.length(); i += 2) {
+            if (Character.isLetterOrDigit(text.charAt(i))) {
+                encryptedText.append(text.charAt(i));
             }
         }
 
-        // Concatenate evenChars and oddChars StringBuilder objects and return the result
-        return evenChars.toString() + oddChars.toString();
+        // Append odd-indexed characters
+        for (int i = 1; i < text.length(); i += 2) {
+            if (Character.isLetterOrDigit(text.charAt(i))) {
+                encryptedText.append(text.charAt(i));
+            }
+        }
+
+        return encryptedText.toString();
     }
 }
